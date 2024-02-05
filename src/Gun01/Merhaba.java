cf399f2f56b45e6ab8a04e9ccfeb0a825ff77a3a
Admin <sidagulov@gmail.com>
2024-02-06 00:52:20 +0200

    Sepet Urun eklenmesi ve kontrolu testi eklendi

---
+++ src/Gun01/Merhaba.java
@@ -0,0 +1,9 @@
+package Gun01;
+
+public class Merhaba {
+    public static void main(String[] args) {
+
+        System.out.println("Merhaba 1");
+        System.out.println("Merhaba 2");
+    }
+}
